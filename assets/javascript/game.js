4b1e854345e97be3996e194107757dd862ca6d27
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-07-14 15:26:42 -0400

    added css reset and crystals 'mother' file

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+/*
+pseudocode:
+math.random a score number
+math. random a number for each crystl div
+borrow code for wins and losses code from psychic game
+make score variable and have it ++ or -- depending on other conditions
+
+*/
