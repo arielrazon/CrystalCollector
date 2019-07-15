0097be20d94b56bbd44e810305148d6ef466bc4a
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-07-14 21:56:55 -0400

    corrected typo in usersScore variable. Project done.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,33 +10,25 @@ make score variable and have it ++ or -- depending on other conditions
 var gameScore = Math.floor(Math.random() * 100) + 25; //sets the score the player must match to random
 
 var crystalOneValue = Math.floor(Math.random() * 25) + 1; //sets crystal 1 value to random
-
 var crystalTwoValue = Math.floor(Math.random() * 25) + 1; //sets crystal 2 value to random
-
 var crystalThreeValue = Math.floor(Math.random() * 25) + 1; //sets crystal 3 value to random
-
 var crystalFourValue = Math.floor(Math.random() * 25) + 1; //sets crystal 4 value to random
-
 var usersScore = 0; //global value of START user score 
 
 var wins = 0; //global value of user wins 
-
 var losses = 0; //global value of user losses 
 
 var reset = function () { //global function for resetting
     newGameScore = Math.floor(Math.random() * 100) + 25;
 
     crystalOneValue = Math.floor(Math.random() * 25) + 1;
-
     crystalTwoValue = Math.floor(Math.random() * 25) + 1;
-
     crystalThreeValue = Math.floor(Math.random() * 25) + 1;
-
     crystalFourValue = Math.floor(Math.random() * 25) + 1;
 
     usersScore = 0;
 
-    $("#scoreBox").text(userScore);
+    $("#scoreBox").text(usersScore);
 };
 const checkScore = () => {
     if (usersScore > gameScore) {
@@ -44,16 +36,14 @@ const checkScore = () => {
         $("#loseText").text("Losses: " + losses);
         $("#scoreBox").text(usersScore);
         reset();
-
-    };
+    }
 
     if (usersScore === gameScore && usersScore !== 0) {
         wins++;
         $("#winsText").text("Wins: " + wins);
         $("#scoreBox").text(usersScore);
         reset();
-
-    };
+    }
 };
 
 $("#goalScore").html(gameScore);//adds gameScore to the html div
