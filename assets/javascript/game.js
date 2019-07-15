1d71ae5db1b45dc4d0a58df83f6741ed9fa612cd
Bob Ross <51304258+happyliltrees@users.noreply.github.com>
2019-07-14 20:29:22 -0400

    added javascript functions to game, added updated styling to CSS sheet - backgrounds, borders to crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,44 +7,55 @@ make score variable and have it ++ or -- depending on other conditions
 
 */
 
-var gameScore = Math.floor(Math.random() * 100) + 25
+var gameScore = Math.floor(Math.random() * 100) + 25 //sets the score the player must match to random
 
-var crystalOneValue = Math.floor(Math.random() * 25) + 1
+var crystalOneValue = Math.floor(Math.random() * 25) + 1 //sets crystal 1 value to random
 
-var crystalTwoValue = Math.floor(Math.random() * 25) + 1
+var crystalTwoValue = Math.floor(Math.random() * 25) + 1 //sets crystal 2 value to random
 
-var crystalThreeValue = Math.floor(Math.random() * 25) + 1
+var crystalThreeValue = Math.floor(Math.random() * 25) + 1 //sets crystal 3 value to random
 
-var crystalFourValue = Math.floor(Math.random() * 25) + 1
+var crystalFourValue = Math.floor(Math.random() * 25) + 1 //sets crystal 4 value to random
 
-var usersScore = 0
+var usersScore = 0 //global value of START user score 
 
-var wins = 0
+var wins = 0 //global value of user wins 
 
-var losses = 0
+var losses = 0 //global value of user losses 
 
-console.log(gameScore)
-console.log(crystalOneValue)
-console.log(crystalTwoValue)
-console.log(crystalThreeValue)
-console.log(crystalFourValue)
+var reset = function () { //global function for resetting
+    gameScore = Math.floor(Math.random() * 100) + 25
 
-$("#goalScore").html(gameScore)
-$("#scoreBox").html(usersScore)
+    crystalOneValue = Math.floor(Math.random() * 25) + 1
+
+    crystalTwoValue = Math.floor(Math.random() * 25) + 1
+
+    crystalThreeValue = Math.floor(Math.random() * 25) + 1
+
+    crystalFourValue = Math.floor(Math.random() * 25) + 1
+
+    userScore = 0
+}
+
+$("#goalScore").html(gameScore)//adds gameScore to the html div
+$("#scoreBox").html(usersScore) // adds user score to the html div
 $("#crystal1").on("click", function () { usersScore = usersScore + crystalOneValue; $("#scoreBox").html(usersScore) })
 $("#crystal2").on("click", function () { usersScore = usersScore + crystalTwoValue; $("#scoreBox").html(usersScore) })
 $("#crystal3").on("click", function () { usersScore = usersScore + crystalThreeValue; $("#scoreBox").html(usersScore) })
 $("#crystal4").on("click", function () { usersScore = usersScore + crystalFourValue; $("#scoreBox").html(usersScore) })
-if (usersScore > gameScore) {
-    losses++
-    usersScore = 0
-    $("#loseText").html("Losses: " + losses)
-    $("#scoreBox").html(usersScore)
 
-}
-if (usersScore = gameScore) {
+if (usersScore === gameScore) {
     wins++
     usersScore = 0
     $("#winsText").html("Wins: " + wins)
     $("#scoreBox").html(usersScore)
+
+    if (usersScore > gameScore) {
+        losses++
+        $("#loseText").html("Losses: " + losses)
+
+        $("#scoreBox").html(usersScore)
+
+    }
+
 }
